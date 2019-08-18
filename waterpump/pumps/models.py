d33af78c2b19871e415bf384a3c581cb94b39297
zeze79 <gowiki1993@gmail.com>
2019-08-18 16:15:07 +0900

    float rate

--- waterpump/pumps/models.py
+++ waterpump/pumps/models.py
@@ -78,9 +78,9 @@ class Sector(TimeStampedModel):
     pump_1_disorder_b = models.CharField(max_length=120, blank=True)
     pump_1_low_water = models.BooleanField(default=False)
     pump_1_auto = models.BooleanField(default=False)  # 수동/자동
-    pump_1_operating_rate = models.CharField(max_length=120, blank=True)  # 가동률
+    #pump_1_operating_rate = models.CharField(max_length=120, blank=True)  # 가동률
     pump_1_current = models.CharField(max_length=120, blank=True)  # 전류
-    pump_1_freq = models.CharField(max_length=120, blank=True)  # 주파수
+    pump_1_freq = models.FloatField(default=0) # 주파수
     pump_1_power = models.CharField(max_length=120, blank=True) # 전력
 
     pump_2_on = models.BooleanField(default=False)
@@ -88,9 +88,9 @@ class Sector(TimeStampedModel):
     pump_2_disorder_b = models.CharField(max_length=120, blank=True)
     pump_2_low_water = models.BooleanField(default=False)
     pump_2_auto = models.BooleanField(default=False)  # 수동/자동
-    pump_2_operating_rate = models.CharField(max_length=120, blank=True)  # 가동률
+    #pump_2_operating_rate = models.CharField(max_length=120, blank=True)  # 가동률
     pump_2_current = models.CharField(max_length=120, blank=True) # 전류
-    pump_2_freq = models.CharField(max_length=120, blank=True)  # 주파수
+    pump_2_freq =models.FloatField(default=0)# 주파수
     pump_2_power = models.CharField(max_length=120, blank=True)  # 전력
 
     pump_3_on = models.BooleanField(default=False)  
@@ -98,9 +98,9 @@ class Sector(TimeStampedModel):
     pump_3_disorder_b = models.CharField(max_length=120, blank=True)
     pump_3_low_water = models.BooleanField(default=False)
     pump_3_auto = models.BooleanField(default=False)  # 수동/자동
-    pump_3_operating_rate = models.CharField(max_length=120, blank=True)  # 가동률
+    #pump_3_operating_rate = models.CharField(max_length=120, blank=True)  # 가동률
     pump_3_current = models.CharField(max_length=120, blank=True)  # 전류
-    pump_3_freq = models.CharField(max_length=120, blank=True)  # 주파수
+    pump_3_freq = models.FloatField(default=0)  # 주파수
     pump_3_power = models.CharField(max_length=120, blank=True)  # 전력
 
     pump_4_on = models.BooleanField(default=False)
@@ -108,15 +108,44 @@ class Sector(TimeStampedModel):
     pump_4_disorder_b = models.CharField(max_length=120, blank=True)
     pump_4_low_water = models.BooleanField(default=False)
     pump_4_auto = models.BooleanField(default=False)  # 수동/자동
-    pump_4_operating_rate = models.CharField(max_length=120, blank=True)  # 가동률
+    #pump_4_operating_rate = models.CharField(max_length=120, blank=True)  # 가동률
     pump_4_current = models.CharField(max_length=120, blank=True)
-    pump_4_freq = models.CharField(max_length=120, blank=True)  # 주파수
+    pump_4_freq = models.FloatField(default=0)  # 주파수
     pump_4_power = models.CharField(max_length=120, blank=True)
 
     #creator = models.ForeignKey(user_models.User, null=True, on_delete='CASCADE')
 
     def __str__(self):
         return '{} - {}'.format(self.town, self.title)
+    
+    def pump_1_operating_rate(self):
+        if self.pump_1_freq != 0:
+            rate = self.pump_1_freq / 60
+            return round(rate, 2)
+        else:
+            return 0
+
+    def pump_2_operating_rate(self):
+        if self.pump_2_freq != 0:
+            rate = self.pump_2_freq / 60
+            return round(rate, 2)
+        else:
+            return 0
+
+    def pump_3_operating_rate(self):
+        if self.pump_3_freq != 0:
+            rate = self.pump_3_freq / 60
+            return round(rate, 2)
+        else:
+            return 0
+
+    def pump_4_operating_rate(self):
+        if self.pump_4_freq != 0:
+
+            rate = self.pump_4_freq / 60
+            return round(rate, 2)
+        else:
+            return 0
 
 
 # @python_2_unicode_compatible

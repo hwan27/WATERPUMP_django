a30130eb6cd067c0999b31274f05fc39a2090c11
hwan27 <meadea27@gmail.com>
2019-08-19 05:01:28 +0900

    icons

--- frontend/src/components/TownComponent/presenter.js
+++ frontend/src/components/TownComponent/presenter.js
@@ -1,28 +1,39 @@
 import React from "react";
 import styles from "./styles.module.scss";
-import wifi from '../../images/wifi01.png';
-import waterTap from '../../images/waterTap.gif';
+import wifi from "../../images/wifi01.png";
+import wifiError from "../../images/wifiError.png";
+import waterTap from "../../images/waterTap.gif";
+import waterTapX from "../../images/waterTapX.png";
 
 const TownComponent = props => {
-    return (
-        <div id="pattern" className={styles.container}>
-            <ul className={styles.list}>
-                <li>
-                    <div onClick={props.click} className={styles.listText}>
-                        <div className={styles.column}>
-                            <div>
-                                {props.title}
-                            </div>
-                            <div className={styles.row}>
-                                <img src={wifi} className={styles.wifi}/>
-                                <img src={waterTap} className={styles.waterTap}/>
-                            </div>
-                        </div>
-                    </div>
-                </li>
-            </ul>
-        </div>
-    );
+  return (
+    <div id="pattern" className={styles.container}>
+      <ul className={styles.list}>
+        <li>
+          <div onClick={props.click} className={styles.listText}>
+            <div className={styles.column}>
+              <div>{props.title}</div>
+              <div className={styles.row}>
+                {props.pump_1_on ||
+                props.pump_2_on ||
+                props.pump_3_on ||
+                props.pump_4_on ? (
+                  <img src={waterTap} className={styles.waterTap} />
+                ) : (
+                  <img src={waterTapX} className={styles.waterTap} />
+                )}
+                {(Date.now() - Date.parse(props.updated_at)) / 3600000 < 2 ? (
+                  <img src={wifi} className={styles.wifi} />
+                ) : (
+                  <img src={wifiError} className={styles.wifi} />
+                )}
+              </div>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default TownComponent;
--- frontend/src/screens/SectorScreen/presenter.js
+++ frontend/src/screens/SectorScreen/presenter.js
@@ -3,70 +3,79 @@ import styles from "./styles.module.scss";
 import SectorComponent from "components/SectorComponent";
 import Navigation from "components/Navigation";
 
-
 const SectorScreen = props => {
-    if (props.loading) {
-        return <LoadingFeed />;
-    } else if (props.sector) {
-        return <RenderFeed {...props} />;
-    }
+  if (props.loading) {
+    return <LoadingFeed />;
+  } else if (props.sector) {
+    return <RenderFeed {...props} />;
+  }
 };
 
 const LoadingFeed = () => null;
 
 const RenderFeed = props => (
-    <div className={styles.container}>
-        <Navigation {...props} title={props.sector.title} />
+  <div className={styles.container}>
+    <Navigation {...props} title={props.sector.title} />
 
-        <div className={styles.feed}>
-            <div className={styles.row}>
-            <div className={styles.modemName}>모뎀번호 : {props.sector.modem_number}</div>
-            <div className={styles.mapButton}>지도</div>
-            
-            <div className={styles.connectBox}>
-                <div className={styles.connect}>
-                    접속상태:{" "}
-                    {props.isRefreshing ? (
-                        <span className={styles.connecting}>접속중</span>
-                    ) : (
-                        <span className={styles.connectEnd}>접속종료</span>
-                    )}
-                </div>
-
-                <div onClick={props.refreshInterval} className={styles.connectPlz}>접속요청</div>
-                </div>
-            </div>
-            <div className={styles.box}>
-                <div className={styles.row1}>
-                    <div className={styles.boxFont}>흡입압력:</div>
-                    <div className={styles.boxValue}>{props.sector.suction_pressure} Bar</div>
-                    <div className={styles.boxFont}>유량:</div>
-                    <div className={styles.boxValue}>{props.sector.discharge} m^3/s</div>
-                </div>
-                <div className={styles.row1}>
-                    <div className={styles.boxFont}>토출압력:</div>
-                    <div className={styles.boxValue}>{props.sector.discharge_pressure} Bar</div>
-                
-                    
-                    <div className={styles.boxFont}>설정압력:</div>
-                    <form>
-                        <input
-                            value={props.setPressure}
-                            onChange={props.set_pressure}
-                            className={styles.boxValueInput}
-    
-                        />
-                    </form>
-                    <div onClick={props.update_pressure} className={styles.boxButton}>입력</div>
-                </div>
-            
-            <div className={styles.updateBox}>최종 업데이트 :  {props.sector.updated_at}</div>
-            </div>
-            <div className={styles.sectorComponent}>
-            <SectorComponent {...props} />
-            </div>
+    <div className={styles.feed}>
+      <div className={styles.row}>
+        <div className={styles.modemName}>
+          모뎀번호 : {props.sector.modem_number}
         </div>
+        <div className={styles.mapButton}>지도</div>
+
+        <div className={styles.connectBox}>
+          <div className={styles.connect}>
+            접속상태:{" "}
+            {props.isRefreshing ? (
+              <span className={styles.connecting}>접속중</span>
+            ) : (
+              <span className={styles.connectEnd}>접속종료</span>
+            )}
+          </div>
+
+          <div onClick={props.refreshInterval} className={styles.connectPlz}>
+            접속요청
+          </div>
+        </div>
+      </div>
+      <div className={styles.box}>
+        <div className={styles.row1}>
+          <div className={styles.boxFont}>흡입압력:</div>
+          <div className={styles.boxValue}>
+            {props.sector.suction_pressure} Bar
+          </div>
+          <div className={styles.boxFont}>유량:</div>
+          <div className={styles.boxValue}>{props.sector.discharge} m^3/s</div>
+        </div>
+        <div className={styles.row1}>
+          <div className={styles.boxFont}>토출압력:</div>
+          <div className={styles.boxValue}>
+            {props.sector.discharge_pressure} Bar
+          </div>
+
+          <div className={styles.boxFont}>설정압력:</div>
+          <form>
+            <input
+              value={props.setPressure}
+              onChange={props.set_pressure}
+              className={styles.boxValueInput}
+            />
+          </form>
+          <div onClick={props.update_pressure} className={styles.boxButton}>
+            입력
+          </div>
+        </div>
+
+        <div className={styles.updateBox}>
+          최종 업데이트 : {props.sector.updated_at}
+        </div>
+      </div>
+      <div className={styles.sectorComponent}>
+        <SectorComponent {...props} />
+      </div>
     </div>
+  </div>
 );
 
 export default SectorScreen;
